3004e670b3dfd3e0424c6bb3868c98bc167ecf90
instamenta <98179343+instamenta@users.noreply.github.com>
2022-08-18 10:39:40 +0300

    still working ...

--- examName/src/controllers/bookController.js
+++ examName/src/controllers/bookController.js
@@ -36,15 +36,43 @@ router.get('/create', isAuthenticated, (req, res) => {
 
 router.post('/create', isAuthenticated, async (req, res) => {
     try {
-        const { title, author, genre, stars, image, review} = req.body
-        console.log(title)
-        console.log(author)
-        console.log(genre)
-        console.log(stars)
-        console.log(image)
-        console.log(review)
+        let { title, author, genre, stars, image, review} = req.body
         
-        //await bookService.create({ ...req.body, owner: req.user._id });
+        let imageState = false
+        imageState = image.startsWith('http://')
+        imageState = image.startsWith('https://')
+        stars = Number(stars)
+        if (title.legth < 2) {
+            throw {
+                message: 'Title must be at least 2 characters long'
+            }
+        }
+        if (author.legth < 5) {
+            throw {
+                message: 'Author must be at least 5 characters long'
+            }
+        }
+        if (genre.legth < 3) {
+            throw {
+                message: 'Genre must be at least 3 characters long'
+            }
+        }
+        if (stars < 0 || stars > 5) {
+            throw {
+                message: 'Stars must be a positive number between 1 - 5'
+            }
+        }
+        if (imageState==false) {
+            throw {
+                message: 'Image must start with http:// or https://'
+            }
+        }
+        if (review.length < 10) {
+            throw {
+                message: 'Review must be at least 10 characters long'
+            }
+        }
+        await bookService.create({ ...req.body, owner: req.user._id });
 
         res.redirect('/books');
     } catch (error) {
